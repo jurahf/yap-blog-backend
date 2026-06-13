4b2d0bd1efc0b8d816c3be3a791ce13607f5e846
Юрий Дубровских <u.dubrovskikh@planfact.io>
2026-06-13 12:59:38 +0500

    Пересоздаем проект на Spriong boot

---
+++ src/main/java/org/blog/YapBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package org.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapBlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YapBlogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/blog/yapblogbackend/YapBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blog.yapblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YapBlogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
